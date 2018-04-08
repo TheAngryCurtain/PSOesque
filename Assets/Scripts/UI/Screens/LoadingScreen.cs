960955361be8fe511d5ceeda3415ff3ad81da6bc
Mike <mike.d.samson@gmail.com>
2018-04-07 23:29:50 -0300

    Fixed up loading screen prefab issues, worked on loading screen game

--- Assets/Scripts/UI/Screens/LoadingScreen.cs
+++ Assets/Scripts/UI/Screens/LoadingScreen.cs
@@ -2,45 +2,109 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UI;
-using UI.Enums;
+using UnityEngine.UI;
 using Rewired;
 
 public class LoadingScreen : UIBaseScreen
 {
 	[SerializeField] private RectTransform m_LoadingIcon;
+    [SerializeField] private UILoadingReticle m_Reticle;
+    [SerializeField] private GameObject m_CollectiblePrefab;
+    [SerializeField] private float m_ReticleSpeed = 5f;
+    [SerializeField] private RectTransform m_StarContainer;
+    [SerializeField] private Text m_CollectedLabel;
 
-	private Vector3 m_Movement = Vector3.zero;
+    private float m_Horizontal;
+    private float m_Vertical;
 
-	protected override void OnInputUpdate(InputActionEventData data)
+    private string m_ScoreKey = "";
+    private int m_BestScore;
+    private int m_TotalCollected = 0;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        m_Reticle.OnCollected += OnCollectibleCollected;
+
+        LoadBestScore();
+        ScatterStars();
+    }
+
+    private void LoadBestScore()
+    {
+        if (PlayerPrefs.HasKey(m_ScoreKey))
+        {
+            m_BestScore = PlayerPrefs.GetInt(m_ScoreKey);
+        }
+        else
+        {
+            m_BestScore = 0;
+        }
+    }
+
+    public override void Shutdown()
+    {
+        base.Shutdown();
+
+        m_Reticle.OnCollected -= OnCollectibleCollected;
+    }
+
+    private void OnCollectibleCollected()
+    {
+        m_TotalCollected += 1;
+        m_CollectedLabel.text = m_TotalCollected.ToString();
+
+        if (m_TotalCollected > m_BestScore)
+        {
+            m_BestScore = m_TotalCollected;
+            PlayerPrefs.SetInt(m_ScoreKey, m_BestScore);
+
+            m_CollectedLabel.color = m_LoadingIcon.GetComponent<Image>().color;
+        }
+
+        SpawnCollectible();
+    }
+
+    private void SpawnCollectible()
+    {
+        GameObject collectible = (GameObject)Instantiate(m_CollectiblePrefab, m_StarContainer);
+        RectTransform rT = collectible.GetComponent<RectTransform>();
+        if (rT != null)
+        {
+            float randX = UnityEngine.Random.Range(-400, 400);
+            float randY = UnityEngine.Random.Range(-225, 225);
+            rT.anchoredPosition = new Vector2(randX, randY);
+        }
+    }
+
+    private void ScatterStars()
+    {
+        int defaultCount = 25;
+        for (int i = 0; i < defaultCount; i++)
+        {
+            SpawnCollectible();
+        }
+    }
+
+    protected override void OnInputUpdate(InputActionEventData data)
 	{
 		if (InputLocked()) return;
 
 		bool handled = false;
-		float value;
 
-		switch (data.actionId)
+        switch (data.actionId)
 		{
-		case RewiredConsts.Action.Navigate_Horizontal:
-			value = data.GetAxis();
-			if (value != 0f)
-			{
-				m_Movement.x = value;
-			}
-			break;
+		    case RewiredConsts.Action.Navigate_Horizontal:
+			    m_Horizontal = data.GetAxis();
+			    break;
 
-		case RewiredConsts.Action.Navigate_Vertical:
-			value = data.GetAxis();
-			if (value != 0f)
-			{
-				m_Movement.y = value;
-			}
-			break;
+		    case RewiredConsts.Action.Navigate_Vertical:
+			    m_Vertical = data.GetAxis();
+			    break;
 		}
 
-		if (m_Movement != Vector3.zero)
-		{
-			m_LoadingIcon.Translate(m_Movement);
-		}
+		m_LoadingIcon.Translate((Vector3.up * m_Vertical + Vector3.right * m_Horizontal) * m_ReticleSpeed * Time.deltaTime, UnityEngine.Space.World);
 
 		// pass to base
 		if (!handled)
---
+++ Assets/Scripts/UI/UILoadingReticle.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILoadingReticle : MonoBehaviour
+{
+    [SerializeField] private RectTransform m_Transform;
+    [SerializeField] private Image m_Image;
+    [SerializeField] private float m_FollowSpeed = 5f;
+    [SerializeField] private float m_MinSqrDist = 10f;
+
+    public System.Action OnCollected;
+
+    private List<RectTransform> m_CollectedStars;
+    private RectTransform m_Followee;
+
+    private void Awake()
+    {
+        m_CollectedStars = new List<RectTransform>();
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < m_CollectedStars.Count; i++)
+        {
+            RectTransform follower = m_CollectedStars[i];
+            if (i == 0)
+            {
+                m_Followee = m_Transform;
+            }
+            else
+            {
+                m_Followee = m_CollectedStars[i - 1];
+            }
+
+            Vector2 direction = (m_Followee.anchoredPosition - follower.anchoredPosition);
+            if (direction.sqrMagnitude > m_MinSqrDist)
+            {
+                follower.anchoredPosition += direction * m_FollowSpeed * Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("UI")) // probably should change this a special layer for this "mode"
+        {
+            RectTransform rT = collision.gameObject.GetComponent<RectTransform>();
+            m_CollectedStars.Add(rT);
+
+            Image image = rT.GetComponent<Image>();
+            image.color = m_Image.color;
+
+            CircleCollider2D collider = rT.GetComponent<CircleCollider2D>();
+            Destroy(collider);
+
+            if (OnCollected != null)
+            {
+                OnCollected();
+            }
+        }
+    }
+}
